a3e09e0ed63f39105f727f93b02733c805c85b15
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-07-03 22:22:45 -0400

    Dice class and testing complete

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,28 +1,33 @@
 import java.util.Random;
 
 public class Dice {
-    private int tossedDies;
-    private final int sixSides;
-
+    private final int tossedDies;
+    private int sixSides;
     public static Random randomNumber = new Random();
 
-    public Dice(int tossedDies){
+    public Dice(int tossedDies) {
         this.tossedDies = tossedDies;
         this.sixSides = 6;
     }
 
-    public int tossAndSum(){
-        int count = 0;
+    public Dice(int tossedDies, int sixSides) {
+        this.tossedDies = tossedDies;
+        this.sixSides = sixSides;
+    }
+
+    public int getTossedDies() {
+        return tossedDies;
+    }
+
+    public int getSixSides() {
+        return sixSides;
+    }
+
+    public int tossAndSum() {
         int sumOfDice = 0;
-        for (int i = 0; i < count; i++){
+        for (int i = 0; i < tossedDies; i++) {
             sumOfDice += randomNumber.nextInt(sixSides) + 1;
         }
         return sumOfDice;
-    }
-    public int getTossedDies(){
-        return tossedDies;
-    }
-    public int getSixSides(){
-        return sixSides;
     }
 }
--- src/main/java/DiceTest.java
+++ src/main/java/DiceTest.java
@@ -1,12 +1,30 @@
-import org.junit.Test;
-import org.junit.Assert;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class DiceTest {
+    @Test
+    public void testDiceOne() {
+        Dice dice;
+        dice = new Dice(2);
+        int results = dice.getTossedDies(); //Act
+        Assertions.assertEquals(2, results); //Assert
+    }
 
     @Test
-    public void testOne(){
-        Dice dice = new Dice(2);
-        int results = dice.getTossedDies(); //Act
-        Assert.assertEquals(2, results); //Assert
+    public void testDiceTwo() {
+        Dice dice;
+        dice = new Dice(2, 5);
+        int results = dice.getSixSides(); //Act
+        Assertions.assertEquals(5, results); //Assert
+    }
+
+    @Test
+    public void testTossedDiesOne() {
+        Dice dice;
+        dice = new Dice(2);
+        int tossSum = dice.tossAndSum(); //Act
+        boolean results = tossSum >= 2 && 12 >= tossSum;
+        Assertions.assertTrue(results); //Assert
     }
 }
