5a7daa6599d0cb24197400699300b1998d3b2380
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-08-09 00:07:58 -0400

    Done

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,7 +1,47 @@
 import java.util.Objects;
 
-public class Simulation {
+public class Simulation{
+    private int dice;
+    private int tosses;
 
-    public static void main(String[] args){
+    private StringBuilder result;
+
+    public Simulation(int tosses) {
+        this.dice = 2;
+        this.tosses = tosses;
+        this.result = new StringBuilder();
+    }
+
+    public int getDice() {
+        return dice;
+    }
+
+    public void setDice(int dice) {
+        this.dice = dice;
+    }
+
+    public int getTosses() {
+        return tosses;
+    }
+
+    public void setTosses(int tosses) {
+        this.tosses = tosses;
+    }
+
+    public StringBuilder getResult(){
+        return result;
+    }
+
+    public void clearResults() {
+        result = new StringBuilder();
+    }
+
+    public void parseResults(Bins bins){
+        clearResults();
+    }
+
+    public void runSimulation(){
+        Dice dice = new Dice(getTosses());
+        Bins bins = new Bins(getDice() * 6);
     }
 }
